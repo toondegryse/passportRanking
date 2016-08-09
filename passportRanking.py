5b96b0315470b97d3511d56f2403c0b8c184b929
Toon Degryse <toon.degryse@gmail.com>
2016-08-09 11:34:30 +0700

    initial commit

---
+++ passportRanking.py
@@ -0,0 +1,45 @@
+# Standard import
+import numpy as np 
+import pandas as pd 
+from pandas import DataFrame, Series
+from pandas.io.json import json_normalize
+from numpy.random import randn
+from cStringIO import StringIO
+import datetime
+import math
+
+# Stats
+from scipy import stats 
+
+# Plotting
+import matplotlib as mpl 
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+# Directory access
+from os import listdir
+from os.path import isfile, join
+
+import urllib2,sys
+from BeautifulSoup import BeautifulSoup, NavigableString
+from bs4 import BeautifulSoup
+
+import json
+
+# json file of countries and their continent https://raw.githubusercontent.com/annexare/Countries/master/countries.json
+df = pd.read_json('countries.json',orient='DataFrame')
+cc = df['countries'].apply(pd.Series)
+
+# countries and the number of countries their passports can access, exported manually to csv 
+#from http://www.atlasandboots.com/best-passport-to-have/
+rankingpd = pd.DataFrame(pd.read_csv('/home/toon/Documents/python_analytics/passportRanking/ranking.csv',header=0,sep=',',parse_dates=True))
+
+# filling the empty rankings and merging the two sources based on country name
+df = rankingpd.fillna(method="ffill")
+cc.columns = ['na','capital','continent','currency','languages','COUNTRY','native','phone']
+newpd = pd.merge(df,cc,on='COUNTRY')
+
+# plot out the countries and continents and their access ranking
+ax = sns.stripplot(x="continent", y="ACCESSIBLE", data=newpd,linewidth=0,hue='COUNTRY',jitter=True);
+ax.legend_.remove()
+plt.show();
